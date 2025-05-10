76d33b0b3742be2cfc636138f6ef3c675dbfb5f2
goodlucksteven <goodlucksteven676@gmail.com>
2025-05-10 01:41:09 +0100

    Update Wevinacoin.sol

--- Wevinacoin.sol
+++ Wevinacoin.sol
@@ -1,1 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract WavinaCoin {
+    string public name = "Wavina Coin";
+    string public symbol = "WNC";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    address public owner;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor(uint256 _initialSupply) {
+        owner = msg.sender;
+        totalSupply = _initialSupply * (10 ** uint256(decimals));
+        balanceOf[owner] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance.");
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value, "Insufficient balance.");
+        require(allowance[_from][msg.sender] >= _value, "Allowance exceeded.");
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        return true;
+    }
+}
